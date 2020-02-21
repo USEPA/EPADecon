fb7daf9db16f2142f0cfe30fd21f3e4cee5b19ce
Kevin Wegman <wegman@battelle.org>
2020-02-20 23:04:15 -0500

    Make unit pass

--- WebServer/ClientApp/tests/unit/example.spec.ts
+++ WebServer/ClientApp/tests/unit/example.spec.ts
@@ -1,8 +1,8 @@
 import { expect } from 'chai';
 
 describe('Example Test', () => {
-  it('will fail', () => {
+  it('will pass', () => {
     const val = 4;
-    expect(val).to.equal(2);
+    expect(val).to.equal(4);
   });
 });
