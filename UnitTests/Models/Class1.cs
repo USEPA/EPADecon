6e88bfba12f645a7f0ce5ab2380a069407600063
Kevin Wegman <wegman@battelle.org>
2020-02-20 23:24:22 -0500

    Added C# unit tests.

---
+++ UnitTests/Models/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace UnitTests.Models
+{
+    class ClientConfigurationTests
+    {
+        [SetUp]
+        void SetUpTests()
+        {
+
+        }
+
+        [Test]
+        void EnsureParse()
+        {
+            Assert.Pass();
+        }
+    }
+}
