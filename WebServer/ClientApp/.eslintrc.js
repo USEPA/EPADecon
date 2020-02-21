8acc6e1734766c061db0bfb45dee55a9c8810166
Kevin Wegman <wegman@battelle.org>
2020-02-20 22:59:29 -0500

    Added in mocha unit tests

--- WebServer/ClientApp/.eslintrc.js
+++ WebServer/ClientApp/.eslintrc.js
@@ -1,17 +1,29 @@
 module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   extends: [
+    'plugin:vue/essential',
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    '@vue/prettier',
   ],
   parserOptions: {
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
   },
   rules: {
     '@typescript-eslint/prefixWithI': 'always',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
   },
+  overrides: [
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      env: {
+        mocha: true,
+      },
+    },
+  ],
 };
---
+++ WebServer/ClientApp/tests/unit/example.spec.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+
+describe('Example Test', () => {
+  it('will fail', () => {
+    const val = 4;
+    expect(val).to.equal(2);
+  });
+});
