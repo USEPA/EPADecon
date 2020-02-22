1cc59b987a03ed6bfeb19d28d0a3f516a2cd8ece
Kevin Wegman <wegman@battelle.org>
2020-02-22 09:37:14 -0500

    [In Progress]
    Starting to add unit tests to the C# code

---
+++ UnitTests/Controllers/ClientConfigurationControllerTests.cs
@@ -0,0 +1,61 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+using NUnit.Framework;
+using WebServer.Controllers;
+using WebServer.Models.ClientConfiguration;
+using WebServer.Services;
+
+namespace UnitTests.Controllers
+{
+    [TestFixture]
+    public class ClientConfigurationControllerTests
+    {
+        private ClientConfiguration TestClientConfiguration { get; set; }
+        private ClientConfigurationController TestController { get; set; }
+
+            [SetUp]
+        public void SetUp()
+        {
+            TestClientConfiguration = new ClientConfiguration()
+            {
+                VuetifySettings = new VuetifyTheme()
+                {
+                    DarkModeEnabled = true,
+                    DisableThemes = true,
+                    ColorSchemes = new VuetifyColorSchemeMap()
+                    {
+                        DarkScheme = new VuetifyColorScheme()
+                        {
+                            Accent = Color.Red,
+                            Anchor = Color.Red,
+                            Error = Color.Red,
+                            Info = Color.Red,
+                            Primary = Color.Red,
+                            Secondary = Color.Red,
+                            Success = Color.Red,
+                            Warning = Color.Red,
+                        }
+                    }
+                }
+            };
+            
+        }
+
+        [Test]
+        public void ControllerProvidesCorrectConfiguration()
+        {
+            // Setup
+            TestController = new ClientConfigurationController(
+                new ClientConfigurationProvider(
+                    TestClientConfiguration));
+
+            // SUT
+            var retrievedConfiguration = TestController.Get();
+
+            // Assert
+            Assert.AreEqual(
+                TestClientConfiguration, 
+                retrievedConfiguration);
+        }
+    }
+}
---
+++ UnitTests/Utility/Json/NewtonsoftJsonColorConverterTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using WebServer.Utility.Json;
+
+namespace UnitTests.Utility.Json
+{
+    [TestFixture]
+    public class NewtonsoftJsonColorConverterTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void ConverterConvertsToHex()
+        {
+            // Setup
+            const string testColorValue = "#eb4034";
+            var testColor = (Color)(new ColorConverter().ConvertFromString(testColorValue) 
+                ?? throw new ApplicationException("Should not be here"));
+
+            // SUT
+            var convertedColorString = JsonConvert.SerializeObject(testColor, new NewtonsoftJsonColorConverter());
+
+            // Assert
+            Assert.True(convertedColorString.Equals($"\"{testColorValue}\"", StringComparison.CurrentCultureIgnoreCase));
+        }
+}
+}
