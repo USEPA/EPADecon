1ae777602227661f4838aa67b1835d4e4a20b338
emily <PERAZA@battelle.org>
2020-08-21 12:08:23 -0400

    First attempt at writing unit test

---
+++ Battelle.EPA.WideAreaDecon.API.Tests/Mocks/ParameterListMock.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+using System.Net.NetworkInformation;
+using Battelle.EPA.WideAreaDecon.API.Models.Parameter;
+
+namespace Battelle.EPA.WideAreaDecon.API.Tests.Mocks
+{
+    public static class ParameterListMock
+    {
+        public static ParameterList GetParameterList()
+        {
+            return new ParameterList()
+            {
+
+            };
+        }
+    }
+}
