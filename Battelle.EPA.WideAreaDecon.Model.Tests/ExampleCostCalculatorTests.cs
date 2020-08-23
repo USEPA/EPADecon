7512603dd624fcda1bff88d3524bc8d5ea651cf5
Kevin Wegman <wegman@battelle.org>
2020-08-23 13:53:55 -0400

    Add in model projects to hold model collateral.

---
+++ Battelle.EPA.WideAreaDecon.Model.Tests/ExampleCostCalculatorTests.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+namespace Battelle.EPA.WideAreaDecon.Model.Tests
+{
+    public class ExampleCostCalculatorTests
+    {
+        private ExampleCostCalculator Calculator { get; set; }
+        [SetUp]
+        public void Setup()
+        {
+            Calculator = new ExampleCostCalculator(10.0, 20.0);
+        }
+
+        [Test]
+        public void CalculateCost()
+        {
+            Assert.AreEqual(30.0, Calculator.CalculateCost(1), 1e-6, "Incorrect cost calculated");
+        }
+    }
+}
---
+++ Battelle.EPA.WideAreaDecon.Model/ExampleCostCalculator.cs
@@ -0,0 +1,19 @@
+﻿namespace Battelle.EPA.WideAreaDecon.Model
+{
+    public class ExampleCostCalculator
+    {
+        private double SomeParameter1 { get; set; }
+        private double SomeParameter2 { get; set; }
+
+        public ExampleCostCalculator(double someParameter1, double someParameter2)
+        {
+            SomeParameter1 = someParameter1;
+            SomeParameter2 = someParameter2;
+        }
+
+        public double CalculateCost(double someOtherInput1)
+        {
+            return (SomeParameter1 + SomeParameter2) * someOtherInput1;
+        }
+    }
+}
