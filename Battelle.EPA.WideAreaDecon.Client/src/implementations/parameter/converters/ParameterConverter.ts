1375a1ed871b859406da918a33dc2ae05c1117ac
Kevin Wegman <wegman@battelle.org>
2020-09-15 19:20:32 -0400

    Finished converter utilizing nelder mead solver

--- Battelle.EPA.WideAreaDecon.Client/src/implementations/parameter/converters/ParameterConverter.ts
+++ Battelle.EPA.WideAreaDecon.Client/src/implementations/parameter/converters/ParameterConverter.ts
@@ -1,7 +1,9 @@
+/* eslint-disable class-methods-use-this */
 import IParameterConverter from '@/interfaces/parameter/IParameterConverter';
 import IParameter from '@/interfaces/parameter/IParameter';
 import ParameterType from '@/enums/parameter/parameterType';
 import * as Utility from '@/mixin/mathUtilityMixin';
+import { nelderMead } from '@/mixin/solverMixin';
 import { injectable } from 'inversify';
 import Uniform from '../distribution/Uniform';
 import TruncatedNormal from '../distribution/TruncatedNormal';
@@ -16,64 +18,76 @@ import LogNormal from '../distribution/LogNormal';
 import NullParameter from '../NullParameter';
 import UniformXDependent from '../distribution/UniformXDependent';
 import Weibull from '../distribution/Weibull';
+import ParameterMetaData from '../ParameterMetaData';
 
 @injectable()
 export default class ParameterConverter implements IParameterConverter {
-  // eslint-disable-next-line class-methods-use-this
   convertToNewType(old: IParameter, newType: ParameterType): IParameter {
     if (old.type === newType) {
       return old;
     }
-    if (isUnivariateDistribution(old)) {
-      const castParam = <IUnivariateParameter>old;
-      switch (newType) {
-        case ParameterType.bimodalTruncatedNormal:
-          return new BimodalTruncatedNormal(
-            old.metaData,
-            castParam.mean,
-            castParam.stdDev,
-            castParam.mean,
-            castParam.stdDev,
-            castParam.min,
-            castParam.max,
-          );
-        case ParameterType.constant:
-          return new Constant(old.metaData, castParam.mean);
-        case ParameterType.efficacy:
-          throw new Error('Not implemented yet'); // TODO: fix upon implementation
-        case ParameterType.logNormal:
-          return new LogNormal(old.metaData, castParam.mean, castParam.stdDev);
-        case ParameterType.logUniform:
-          return new LogUniform(
-            old.metaData,
-            Utility.convertToLog10(castParam.min),
-            Utility.convertToLog10(castParam.max),
-          );
-        case ParameterType.null:
-          return new NullParameter();
-        case ParameterType.pert:
-          return new BetaPERT(old.metaData, castParam.min, castParam.max, castParam.mode);
-        case ParameterType.truncatedLogNormal:
-          return new TruncatedLogNormal(
-            old.metaData,
-            Utility.convertToLog10(castParam.min),
-            Utility.convertToLog10(castParam.max),
-            Utility.convertToLog10(castParam.mean),
-            Utility.convertToLog10(castParam.stdDev),
-          );
-        case ParameterType.truncatedNormal:
-          return new TruncatedNormal(old.metaData, castParam.min, castParam.max, castParam.mean, castParam.stdDev);
-        case ParameterType.uniform:
-          return new Uniform(old.metaData, castParam.min, castParam.max);
-        case ParameterType.uniformXDependent:
-          return new UniformXDependent(old.metaData);
-        case ParameterType.weibull:
-          return new Weibull(old.metaData);
-        default:
-          throw new Error('New type not recognized');
-      }
-    }
+    return isUnivariateDistribution(old)
+      ? this.convertFromUnivariate(<IUnivariateParameter>old, newType)
+      : this.convertFromNonUnivariate(old, newType);
+  }
 
+  convertFromUnivariate(old: IUnivariateParameter, newType: ParameterType): IParameter {
+    switch (newType) {
+      case ParameterType.bimodalTruncatedNormal:
+        return new BimodalTruncatedNormal(old.metaData, old.mean, old.stdDev, old.mean, old.stdDev, old.min, old.max);
+      case ParameterType.constant:
+        return new Constant(old.metaData, old.mean);
+      case ParameterType.efficacy:
+        throw new Error('Not implemented yet'); // TODO: fix upon implementation
+      case ParameterType.logNormal:
+        return new LogNormal(old.metaData, old.mean, old.stdDev);
+      case ParameterType.logUniform:
+        return new LogUniform(old.metaData, Utility.convertToLog10(old.min), Utility.convertToLog10(old.max));
+      case ParameterType.null:
+        return new NullParameter();
+      case ParameterType.pert:
+        return new BetaPERT(old.metaData, old.min, old.max, old.mode);
+      case ParameterType.truncatedLogNormal:
+        return new TruncatedLogNormal(
+          old.metaData,
+          Utility.convertToLog10(old.min),
+          Utility.convertToLog10(old.max),
+          Utility.convertToLog10(old.mean),
+          Utility.convertToLog10(old.stdDev),
+        );
+      case ParameterType.truncatedNormal:
+        return new TruncatedNormal(old.metaData, old.min, old.max, old.mean, old.stdDev);
+      case ParameterType.uniform:
+        return new Uniform(old.metaData, old.min, old.max);
+      case ParameterType.uniformXDependent:
+        return new UniformXDependent(old.metaData);
+      case ParameterType.weibull: {
+        if (old.mean === undefined || old.stdDev === undefined) {
+          return new Weibull(old.metaData);
+        }
+
+        const actMean = old.mean;
+        const actVar = old.stdDev ** 2;
+        const minimize = (values: number[]): number => {
+          const actual = new Weibull(new ParameterMetaData(), values[0], values[1]);
+
+          if (!!actual.mean && !!actual.variance) {
+            return Math.sqrt((actual.mean - actMean) ** 2 + (actual.variance - actVar ** 2) ** 2);
+          }
+          return Infinity;
+        };
+        const guess = [1, 1];
+
+        // SUT
+        const sln = nelderMead(minimize, guess);
+        return new Weibull(old.metaData, sln.Input[0], sln.Input[1]);
+      }
+      default:
+        throw new Error('New type not recognized');
+    }
+  }
+
+  convertFromNonUnivariate(old: IParameter, newType: ParameterType): IParameter {
     switch (newType) {
       case ParameterType.bimodalTruncatedNormal:
         return new BimodalTruncatedNormal(old.metaData);
