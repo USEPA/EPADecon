0134fa9da1282c235b32671dcb0927e1631f0c71
Eli Detmers <DETMERS@battelle.org>
2020-09-17 12:58:04 -0400

    Removed "CS" from classes and file names
    removed "CS"

---
+++ Battelle.EPA.WideAreaDecon.Model/LaborCostCalculator.cs
@@ -0,0 +1,51 @@
+﻿using System.Runtime.Remoting.Lifetime;
+
+namespace Battelle.EPA.WideAreaDecon.Model
+{
+	public class LaborCostCalculator
+	{
+		private double NumTeams { get; set; }
+		private double PersonnelOverhead { get; set; }
+		private double NumEntriesPerTeamPerDay { get; set; }
+		private double HoursPerEntryPerTeam { get; set; }
+		private double HoursPerExitPerTeam { get; set; }
+
+		public LaborCostCalculator(double numTeams, double personnelOverhead, double numEntriesPerTeamPerDay, double hoursPerEntryPerTeam, double hoursPerExitPerTeam)
+		{
+			NumTeams = numTeams;
+			PersonnelOverhead = personnelOverhead;
+			NumEntriesPerTeamPerDay = numEntriesPerTeamPerDay;
+			HoursPerEntryPerTeam = hoursPerEntryPerTeam;
+			HoursPerExitPerTeam = hoursPerExitPerTeam;
+
+		}
+
+		SuppliesCostCalculatorCS suppliesCostCalculatorCS = new SuppliesCostCalculatorCS;
+		public double WorkDays = suppliesCostCalculatorCS.CalculateWorkDays();
+
+		public double CalculateLaborCost(double RoundTripDays, double[] PersonnelReqPerTeam, double[] PersonnelHourlyRate)
+		{
+
+			double LaborHoursCost = 0;
+
+			for (int i = 0; i < PersonnelReqPerTeam.Length; i++)
+            {
+				LaborHoursCost = LaborHoursCost + (PersonnelReqPerTeam[i] * PersonnelHourlyRate[i]);
+            }
+
+			return ((WorkDays + PersonnelOverhead + RoundTripDays) * 8) * NumTeams * (LaborHoursCost);
+
+		}
+
+		public double CalculateEntExitLaborCost(double[] PersonnelReqPerTeam, double[] PersonnelHourlyRate)
+        {
+			double LaborHoursCost = 0;
+
+			for (int i = 0; i < PersonnelReqPerTeam.Length; i++)
+			{
+				LaborHoursCost = LaborHoursCost + (PersonnelReqPerTeam[i] * PersonnelHourlyRate[i]);
+			}
+			return ((Workdays * NumEntriesPerTeamPerDay * NumTeams * HoursPerEntryPerTeam) + (Workdays * NumEntriesPerTeamPerDay * NumTeams * HoursPerExitPerTeam)) * (LaborHoursCost);
+		}
+	}
+}
---
+++ Battelle.EPA.WideAreaDecon.Model/SuppliesCostCalculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Battelle.EPA.WideAreaDecon.Model
+{ 
+    public class SuppliesCostCalculator
+    {
+        private double NumTeams { get; set; }
+        private double SqFtPerWipe { get; set; }
+        private double SqFtPerHEPASock { get; set; }
+        private double CostPerWipe { get; set; }
+        private double CostPerVacuum { get; set; }
+        private double HEPARentalCostPerDay { get; set; }
+        private double HEPASocksPerHrPerTeam { get; set; }
+        private double SqFtToBeWiped { get; set; }
+        private double SqFtToBeHEPA { get; set; }
+
+        public SuppliesCostCalculator(double numTeams, double sqFtPerWipe, double sqFtPerHEPASock, double costPerWipe, double costPerVacuum, double hepaRentalCostPerDay, double hepaSocksPerHrPerTeam)
+        {
+            NumTeams = numTeams;
+            SqFtPerWipe = sqFtPerWipe;
+            SqFtPerHEPASock = sqFtPerHEPASock;
+            CostPerWipe = costPerWipe;
+            CostPerVacuum = costPerVacuum;
+            HEPARentalCostPerDay = hepaRentalCostPerDay;
+            HEPASocksPerHrPerTeam = hepaSocksPerHrPerTeam;
+        }
+
+    public double CalculateSuppliesCost()
+    {
+        return ((SqFtToBeWiped / SqFtPerWipe) * CostPerwipe) + ((SqFtToBeHEPA / SqFtPerHEPASock) * CostPerVacuum) + (((SqFtToBeHEPA / SqFtPerHEPASock) / (HEPASocksPerHr * NumTeams * 8)) * HEPARentalCostPerDay);
+    }
+
+    public double CalculateWorkDays()
+    {
+        return (Math.abs(((SqFtToBeWiped / SqFtPerWipe) / (WipesPerHr * NumTeams)) / 8) + Math.Abs(((SqFtToBeHEPA / SqFtPerHEPASock) / (HEPASocksPerHrPerTeam * NumTeams)) / 8));
+    }
+    }
+}
