c1255947ceaad6a3be3febd116e64ef455469fb9
Eli Detmers <DETMERS@battelle.org>
2020-09-16 21:45:28 -0400

    Separate CS into subclasses
    More manageable classes, in separate files. Original file remains for reference until subclasses are complete.

---
+++ Battelle.EPA.WideAreaDecon.Model/LaborCostCalculatorCS.cs
@@ -0,0 +1,33 @@
+﻿namespace Battelle.EPA.WideAreaDecon.Model
+{
+	public class LaborCostCalculatorCS
+	{
+		private double NumTeams { get; set; }
+		private double PersonnelOverhead { get; set; }
+		private double[] PersonnelReqPerTeam { get; set; }
+
+		public LaborCostCalculatorCS(double numTeams, double personnelOverhead, double[] personnelReqPerTeam)
+		{
+			NumTeams = numTeams;
+			PersonnelOverhead = personnelOverhead;
+			PersonnelReqPerTeam = personnelReqPerTeam;
+
+		}
+
+		public double CalculateLaborCost(double SqFtToBeWiped, double SqFtToBeHEPA, double RoundTripDays, double[]PersonnelHourlyRate)
+		{
+			SuppliesCostCalculatorCS suppliesCostCalculatorCS = new SuppliesCostCalculatorCS;
+			double WorkDays = suppliesCostCalculatorCS.CalculateWorkDays(SqFtToBeWiped, SqFtToBeHEPA);
+
+			double LaborHoursCost = 0;
+
+			for (int i = 0; i < PersonnelReqPerTeam.Length; i++)
+            {
+				LaborHoursCost = LaborHoursCost + (PersonnelReqPerTeam[i] * PersonnelHourlyRate[i]);
+            }
+
+			return ((WorkDays + PersonnelOverhead + RoundTripDays) * 8) * NumTeams * (LaborHoursCost);
+
+		}
+	}
+}
---
+++ Battelle.EPA.WideAreaDecon.Model/SuppliesCostCalculatorCS.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Runtime.Remoting.Messaging;
+
+namespace Battelle.EPA.WideAreaDecon.Model
+{ 
+    public class SuppliesCostCalculatorCS
+    {
+        private double NumTeams { get; set; }
+        private double SqFtPerWipe { get; set; }
+        private double SqFtPerHEPASock { get; set; }
+        private double CostPerWipe { get; set; }
+        private double CostPerVacuum { get; set; }
+        private double HEPARentalCostPerDay { get; set; }
+        private double HEPASocksPerHourPerTeam { get; set; }
+
+        public SuppliesCostCalculatorCS(double numTeams, double sqFtPerWipe, double sqFtPerHEPASock, double costPerWipe, double costPerVacuum, double hepaRentalCostPerDay, double hepaSocksPerHourPerTeam)
+        {
+            NumTeams = numTeams;
+            SqFtPerWipe = sqFtPerWipe;
+            SqFtPerHEPASock = sqFtPerHEPASock;
+            CostPerWipe = costPerWipe;
+            CostPerVacuum = costPerVacuum;
+            HEPARentalCostPerDay = hepaRentalCostPerDay;
+            HEPASocksPerHourPerTeam = hepaSocksPerHourPerTeam;
+        }
+
+    public double CalculateSuppliesCost(double SqFtToBeWiped, double SqFtToBeHEPA)
+    {
+        return ((SqFtToBeWiped / SqFtPerWipe) * CostPerwipe) + ((SqFtToBeHEPA / SqFtPerHEPASock) * CostPerVacuum) + (((SqFtToBeHEPA / SqFtPerHEPASock) / (HEPASocksPerHr * NumTeams * 8)) * HEPARentalCostPerDay);
+    }
+    }
+}
