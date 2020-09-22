3c6e636fa0e58acf779dd07fc9d6322b5a935b0d
Eli Detmers <DETMERS@battelle.org>
2020-09-22 10:33:10 -0400

    Decon supplies cost, removed IndoorUnderground
    Finished decontamination, removed original large file.

---
+++ Battelle.EPA.WideAreaDecon.Model/Decontamination/SuppliesCostCalculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Battelle.EPA.WideAreaDecon.Model.Decontamination
+{
+	public class SuppliesCostCalculator
+	{
+		private double DeconAgentCostPerGallon { get; set; }
+		private double DeconMaterialsCost { get; set; }
+		private double DeconAgentVolume { get; set; }
+		private double[] DeconAgentVolumeBySurface{ get; set; }
+
+		public SuppliesCostCalculator(double deconAgentCostPerGallon, double deconMaterialsCost, double deconAgentVolume, double[] deconAgentVolumeBySurface)
+		{
+			DeconAgentCostPerGallon - deconAgentCostPerGallon;
+			DeconMaterialsCost = deconMaterialsCost;
+			DeconAgentVolume = deconAgentVolume;
+			DeconAgentVolumeBySurface = deconAgentVolumeBySurface;
+		}
+
+		public double CalculateSuppliesCost(double RoomVolume, double[] PercentOfRoomBySurface, bool Fogging)
+        {
+			if (Fogging)
+            {
+				return (DeconMaterialsCost + (RoomVolume * DeconAgentVolume * DeconAgentCostPerGallon));
+            }
+			else
+            {
+				double AgentNeededPerTreatment = 0;
+				for(int i = 0; i )
+                {
+					AgentNeededPerTreatment = AgentNeededPerTreatment + (DeconAgentVolumeBySurface[i] * PercentOfRoomBySurface[i]);
+                }
+				return (DeconMaterialsCost + (AgentNeededPerTreatment * DeconAgentCostPerGallon));
+
+			}
+        }
+	}
+}
