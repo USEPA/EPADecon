ab5a362703c27bd011d701d8abf3d24e67af79f2
Eli Detmers <DETMERS@battelle.org>
2020-09-29 12:51:20 -0400

    Supplies Cost unit test (characterization sampling)
    Supplies Cost unit test (characterization sampling) incomplete

---
+++ Battelle.EPA.WideAreaDecon.Model.Tests/CharacterizationSampling/SuppliesCostCalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Battelle.EPA.WideAreaDecon.Model.Tests.CharacterizationSampling
+{
+    public class SuppliesCostCalculatorTests
+    {
+        private SuppliesCostCalculator Calculator { get; set; }
+        [SetUp]
+        public void Setup()
+        {
+            Calculator = new SuppliesCostCalculator(4.0, 10.0, 15.0,  );
+        }
+
+        [Test]
+        public void CalculateCost()
+        {
+            Assert.AreEqual(30.0, Calculator.CalculateSuppliesCost(1), 1e-6, "Incorrect cost calculated");
+        }
+    }
+}
