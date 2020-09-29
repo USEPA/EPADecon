2e2b1f3ecc5080241df5885259b271500482f66d
Eli Detmers <DETMERS@battelle.org>
2020-09-29 14:24:51 -0400

    Supplies minor change, added Labor
    Fixed supplies, added and completed labor cost unit tests (3 tests in labor)

---
+++ Battelle.EPA.WideAreaDecon.Model.Tests/CharacterizationSampling/LaborCostCalculatorTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Battelle.EPA.WideAreaDecon.Model.CharacterizationSampling
+
+namespace Battelle.EPA.WideAreaDecon.Model.Tests.CharacterizationSampling
+{
+    public class LaborCostCalculatorTests
+    {
+        private LaborCostCalculator Calculator { get; set; }
+        [SetUp]
+        public void Setup()
+        {
+            Calculator = new LaborCostCalculator(5.0, { 4.0, 6.0, 5.0, 4.0 }, 10, 2, .5, .5, { 10.0, 8.0, 15.0, 20.0});
+        }
+
+        [Test]
+        public void CalculateCost()
+        {
+            Assert.AreEqual((149040.0), Calculator.CalculateLaborCost(5), 1e-6, "Incorrect labor cost calculated");
+            Assert.AreEqual((729.0), Calculator.CalculateEntExitLaborCost(), 1e-6, "Incorrect ent/exit labor cost calculated");
+            Assert.AreEqual((46/3), Calculator.CalculateLaborDays(5), 1e-6, "Incorrect labor days calculated");
+        }
+    }
+}
--- Battelle.EPA.WideAreaDecon.Model.Tests/CharacterizationSampling/SuppliesCostCalculatorTests.cs
+++ Battelle.EPA.WideAreaDecon.Model.Tests/CharacterizationSampling/SuppliesCostCalculatorTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using Battelle.EPA.WideAreaDecon.Model.CharacterizationSampling
 
 namespace Battelle.EPA.WideAreaDecon.Model.Tests.CharacterizationSampling
 {
@@ -8,13 +9,14 @@ namespace Battelle.EPA.WideAreaDecon.Model.Tests.CharacterizationSampling
         [SetUp]
         public void Setup()
         {
-            Calculator = new SuppliesCostCalculator(4.0, 10.0, 15.0,  );
+            Calculator = new SuppliesCostCalculator(5.0, 10.0, 15.0, 15.0, 10.0, 0.25, 0.5, 20.0, 1000.0, 1000.0);
         }
 
         [Test]
         public void CalculateCost()
         {
-            Assert.AreEqual(30.0, Calculator.CalculateSuppliesCost(1), 1e-6, "Incorrect cost calculated");
+            Assert.AreEqual((185 / 3), Calculator.CalculateSuppliesCost(), 1e-6, "Incorrect supplies cost calculated");
+            Assert.AreEqual((1 / 3), Calculator.CalculateWorkDays(), 1e-6, "Incorrect work days cost calculated");
         }
     }
 }
