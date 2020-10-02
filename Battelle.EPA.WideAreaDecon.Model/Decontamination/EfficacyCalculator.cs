66ec861fe29914bd556d5546782af3ae7788f15a
emily <PERAZA@battelle.org>
2020-10-02 16:00:46 -0400

    Added an efficacy calculator class

---
+++ Battelle.EPA.WideAreaDecon.Model/Decontamination/EfficacyCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Battelle.EPA.WideAreaDecon.Model.Decontamination
+{
+    class EfficacyCalculator
+    {
+        private readonly double[] InitialSporeLoading;
+        private readonly string[] SurfaceTypes;
+        private readonly string[] AppMethodBySurfaceType;
+        private readonly double[] DesiredSporeThreshold;
+        private readonly double[] TreatmentDaysPerAM;
+
+        public EfficacyCalculator()
+        {
+
+        }
+
+        public EfficacyCalculator(double[] initialSporeLoading, string[] surfaceTypes, string[] applicationMethods, double[] desiredSporeThreshold, double[] treatmentDaysPerAM)
+        {
+            InitialSporeLoading = initialSporeLoading;
+            SurfaceTypes = surfaceTypes;
+            AppMethodBySurfaceType = applicationMethods;
+            DesiredSporeThreshold = desiredSporeThreshold;
+            TreatmentDaysPerAM = treatmentDaysPerAM;
+        }
+
+        public double CalculateEfficacy()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
