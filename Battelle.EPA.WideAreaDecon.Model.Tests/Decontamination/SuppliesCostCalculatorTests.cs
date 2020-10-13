a46e4f1f56cf020b8d1c89536a81bda7a0ba1625
Eli Detmers <DETMERS@battelle.org>
2020-10-13 16:05:36 -0400

    Decon supplies fix, unused varialbe removed,
    minor fixes

--- Battelle.EPA.WideAreaDecon.Model.Tests/Decontamination/SuppliesCostCalculatorTests.cs
+++ Battelle.EPA.WideAreaDecon.Model.Tests/Decontamination/SuppliesCostCalculatorTests.cs
@@ -13,16 +13,14 @@ namespace Battelle.EPA.WideAreaDecon.Model.Tests.Decontamination
         {
             var deconAgentVolumeBySurface = new Dictionary<SurfaceType, double>()
             {
-                { SurfaceType.S1, 0.0 }
+                { SurfaceType.S1, 1.0 }
             };
             var deconAgentCostPerVolume = 0.52306056;
             var deconMaterialsCost = 1.53612754751869;
-            double totalRoomSa = 100;
             var deconAgentVolume = 0.3342015463;
             Calculator = new SuppliesCostCalculator(
                 deconAgentCostPerVolume,
                 deconMaterialsCost,
-                totalRoomSa,
                 deconAgentVolume,
                 deconAgentVolumeBySurface
             );
@@ -33,16 +31,16 @@ namespace Battelle.EPA.WideAreaDecon.Model.Tests.Decontamination
         {
             var percentOfRoomBySurface = new Dictionary<SurfaceType, double>()
             {
-                { SurfaceType.S1, 0.0 }
+                { SurfaceType.S1, 1.0 }
             };
             double roomVolume = 25000;
 
-            Assert.AreEqual(4523.80395376547,
+            Assert.AreEqual(2.059188108,
                 Calculator.NonFoggingSuppliesCostCalculator(percentOfRoomBySurface), 1e-6,
-                "Incorrect cost calculated(fogging)");
-            Assert.AreEqual(153.612754751869,
+                "Incorrect cost calculated(non fog)");
+            Assert.AreEqual(4371.727327,
                 Calculator.FoggingSuppliesCostCalculator(roomVolume), 1e-6,
-                "Incorrect cost calculated(surface)");
+                "Incorrect cost calculated(fogging)");
         }
     }
 }
--- Battelle.EPA.WideAreaDecon.Model/Decontamination/SuppliesCostCalculator.cs
+++ Battelle.EPA.WideAreaDecon.Model/Decontamination/SuppliesCostCalculator.cs
@@ -9,18 +9,15 @@ namespace Battelle.EPA.WideAreaDecon.Model.Decontamination
         private readonly double _deconAgentVolume;
         private readonly Dictionary<SurfaceType, double> _deconAgentVolumeBySurface;
         private readonly double _deconMaterialsCost;
-        private readonly double _totalRoomSurfaceArea;
 
         public SuppliesCostCalculator(
             double deconAgentCostPerVolume,
             double deconMaterialsCost,
-            double totalRoomSurfaceArea,
             double deconAgentVolume,
             Dictionary<SurfaceType, double> deconAgentVolumeBySurface)
         {
             _deconAgentCostPerVolume = deconAgentCostPerVolume;
             _deconMaterialsCost = deconMaterialsCost;
-            _totalRoomSurfaceArea = totalRoomSurfaceArea;
             _deconAgentVolume = deconAgentVolume;
             _deconAgentVolumeBySurface = deconAgentVolumeBySurface;
         }
@@ -28,12 +25,12 @@ namespace Battelle.EPA.WideAreaDecon.Model.Decontamination
         public double NonFoggingSuppliesCostCalculator (Dictionary<SurfaceType, double> percentOfRoomBySurface)
         {
             var agentNeededPerTreatment = _deconAgentVolumeBySurface.Values.Zip(percentOfRoomBySurface.Values, (x, y) => x * y).Sum();
-            return _deconMaterialsCost * _totalRoomSurfaceArea + agentNeededPerTreatment * _deconAgentCostPerVolume;
+            return _deconMaterialsCost + ((agentNeededPerTreatment) * _deconAgentCostPerVolume);
         }
 
         public double FoggingSuppliesCostCalculator (double roomVolume)
         {
-            return _deconMaterialsCost * _totalRoomSurfaceArea + roomVolume * _deconAgentVolume * _deconAgentCostPerVolume;
+            return _deconMaterialsCost + (roomVolume * _deconAgentVolume * _deconAgentCostPerVolume);
         }
     }
 }
