fb6908710e075e63bd3f0db86f78c46becde945b
emily <PERAZA@battelle.org>
2020-11-09 20:35:09 -0500

    Fixed incident command calculations

--- Battelle.EPA.WideAreaDecon.Model/GlobalConstants.cs
+++ Battelle.EPA.WideAreaDecon.Model/GlobalConstants.cs
@@ -5,6 +5,7 @@ namespace Battelle.EPA.WideAreaDecon.Model
     public static class GlobalConstants
     {
         public static readonly double HoursPerWorkDay = 8;
-        public static readonly double AssumedDriverSpeed = 55;
+        public static readonly double AssumedDriverSpeed = 88.5139; // km/hour (55 mph)
+        public static readonly double MinutesToDays = 1440.0;
     }
 }
--- Battelle.EPA.WideAreaDecon.Model/IncidentCommand/LaborCostCalculator.cs
+++ Battelle.EPA.WideAreaDecon.Model/IncidentCommand/LaborCostCalculator.cs
@@ -7,6 +7,7 @@ namespace Battelle.EPA.WideAreaDecon.Model.IncidentCommand
     public class LaborCostCalculator : ILaborCostCalculator
     {
         private readonly CharacterizationSampling.ILaborCostCalculator _laborCostCalculatorCs;
+        private readonly CharacterizationSampling.IPhaseLagCalculator _phaseLagCalculator;
         private readonly Decontamination.ILaborCostCalculator _laborCostCalculatorDc;
         private readonly SourceReduction.ILaborCostCalculator _laborCostCalculatorSr;
         private readonly Dictionary<PersonnelLevel, double> _personnelHourlyRate;
@@ -18,6 +19,7 @@ namespace Battelle.EPA.WideAreaDecon.Model.IncidentCommand
             double personnelOverheadDays,
             Dictionary<PersonnelLevel, double> personnelHourlyRate,
             CharacterizationSampling.ILaborCostCalculator laborCostCalculatorCs,
+            CharacterizationSampling.IPhaseLagCalculator phaseLagCalculator,
             SourceReduction.ILaborCostCalculator laborCostCalculatorSr,
             Decontamination.ILaborCostCalculator laborCostCalculatorDc)
         {
@@ -25,6 +27,7 @@ namespace Battelle.EPA.WideAreaDecon.Model.IncidentCommand
             _personnelOverheadDays = personnelOverheadDays;
             _personnelHourlyRate = personnelHourlyRate;
             _laborCostCalculatorCs = laborCostCalculatorCs;
+            _phaseLagCalculator = phaseLagCalculator;
             _laborCostCalculatorSr = laborCostCalculatorSr;
             _laborCostCalculatorDc = laborCostCalculatorDc;
         }
@@ -37,26 +40,25 @@ namespace Battelle.EPA.WideAreaDecon.Model.IncidentCommand
             var laborDaysSr = _laborCostCalculatorSr.CalculateLaborDays( _numberTeams, personnelRoundTripDays, surfaceAreaToBeSourceReduced);
             var laborDaysDc = _laborCostCalculatorDc.CalculateLaborDays(personnelRoundTripDays);
 
-            return laborDaysCs + laborDaysSr + laborDaysDc + _personnelOverheadDays;
+            var lagTime = _phaseLagCalculator.CalculatePhaseLagTime(numberLabs, sampleTimeTransmitted, _surfaceAreaToBeWiped, _surfaceAreaToBeHepa);
+
+            return laborDaysCs + lagTime + laborDaysSr + laborDaysDc + _personnelOverheadDays;
         }
 
         public double CalculateLaborCost(double _numberTeams, double surfaceAreaToBeSourceReduced, double personnelRoundTripDays,
             double _surfaceAreaToBeHepa,double _surfaceAreaToBeWiped, int numberLabs, double sampleTimeTransmitted)
         {
-            var laborDaysCs = _laborCostCalculatorCs.CalculateLaborDays(  _numberTeams, personnelRoundTripDays, _surfaceAreaToBeHepa,
-                _surfaceAreaToBeWiped, numberLabs, sampleTimeTransmitted);
-            var laborDaysSr = _laborCostCalculatorSr.CalculateLaborDays( _numberTeams,  surfaceAreaToBeSourceReduced, personnelRoundTripDays);
-            var laborDaysDc = _laborCostCalculatorDc.CalculateLaborDays(personnelRoundTripDays);
-
+            
             var totalPersonnel = _personnelReqPerTeam.Values.Sum();
 
             var personnelHoursCost = _personnelReqPerTeam.Values.Zip(_personnelHourlyRate.Values, (x, y) => x * y).Sum();
 
-            var laborHours = GlobalConstants.HoursPerWorkDay * (laborDaysCs + laborDaysSr + laborDaysDc + _personnelOverheadDays +
-                personnelRoundTripDays);
+            var onSiteDays = CalculateOnSiteDays(_numberTeams, surfaceAreaToBeSourceReduced, personnelRoundTripDays,
+             _surfaceAreaToBeHepa, _surfaceAreaToBeWiped, numberLabs, sampleTimeTransmitted);
 
-            return(GlobalConstants.HoursPerWorkDay * (CalculateOnSiteDays( _numberTeams,  surfaceAreaToBeSourceReduced,  personnelRoundTripDays,
-             _surfaceAreaToBeHepa, _surfaceAreaToBeWiped,  numberLabs,  sampleTimeTransmitted)));
+            var laborHours = GlobalConstants.HoursPerWorkDay * (onSiteDays + personnelRoundTripDays);
+
+            return(laborHours * personnelHoursCost);
         }
     }
 }
