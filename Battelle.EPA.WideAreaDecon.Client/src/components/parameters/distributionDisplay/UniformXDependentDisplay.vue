f831b9c4b626a41d7a71eed57f78e39e282acbd5
Daniel Sharp <sharpd@battelle.org>
2021-01-21 15:45:35 -0500

    updates to uni x dep display

--- Battelle.EPA.WideAreaDecon.Client/src/components/parameters/distributionDisplay/UniformXDependentDisplay.vue
+++ Battelle.EPA.WideAreaDecon.Client/src/components/parameters/distributionDisplay/UniformXDependentDisplay.vue
@@ -2,7 +2,7 @@
   <v-container :style="vuetifyColorProps()">
     <v-row>
       <v-col>
-        <v-slider v-model="sliderX" :max="max" :min="min" :step="step" thumb-label>
+        <v-slider v-model="sliderValue" :max="max" :min="min" :step="step" thumb-label>
           <template v-slot:prepend>
             <p class="grey--text">{{ min }}</p>
           </template>
@@ -13,24 +13,40 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col align="center" v-if="false">
-        <v-btn-toggle dense background-color="primary" multiple>
-          <v-btn @click="toggleLoading">Loading </v-btn>
-          <v-btn @click="toggleTemp">Temp </v-btn>
+      <v-col cols="4" class="mr-auto">
+        <v-card class="pa-2" outlined tile>
+          <v-text-field
+            ref="value"
+            @keydown="onTextEnterPressed"
+            @blur="updateOnTextChange"
+            v-model="textValue"
+            label="Value"
+            :rules="[validationRules]"
+            hide-details="auto"
+          >
+            <template v-slot:append>
+              <p class="grey--text">{{ parameterValue.metaData.units }}</p>
+            </template>
+          </v-text-field>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col align="center">
+        <v-btn-toggle v-model="selectedSetName" dense mandatory background-color="primary">
+          <v-btn v-for="set in variableSets" :key="set.name" :value="set.name">{{ set.name }}</v-btn>
         </v-btn-toggle>
       </v-col>
     </v-row>
-    <v-card flat class="pa-5" tile width="100%" height="400">
-      <scatter-plot-wrapper :options="chartOptions" :data="exampleData" :type="'scatter'" :width="400" :height="150" />
+    <v-card v-if="displayChart" flat class="pa-5" tile width="100%" height="400">
+      <scatter-plot-wrapper :options="chartOptions" :data="chartData" type="scatter" :width="400" :height="150" />
     </v-card>
   </v-container>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import { Component, Prop } from 'vue-property-decorator';
-// import { Uniform } from 'battelle-common-typescript-external-rmath';
-// import { UniformDistribution } from 'battelle-common-typescript-statistics';
+import { Component, Prop, Watch } from 'vue-property-decorator';
 import IParameterDisplay from '@/interfaces/component/IParameterDisplay';
 import UniformXDependent from '@/implementations/parameter/distribution/UniformXDependent';
 import {
@@ -41,18 +57,26 @@ import {
   DefaultChartOptions,
   DefaultChartData,
 } from 'battelle-common-vue-charting/src';
+import { Key } from 'ts-keycode-enum';
+
+interface VariableSet {
+  name: string;
+  indices: number[];
+  points: number[];
+  value: number | undefined;
+}
 
 @Component({ components: { ScatterPlotWrapper } })
 export default class UniformXDependentDisplay extends Vue implements IParameterDisplay {
   @Prop({ required: true }) parameterValue!: UniformXDependent;
 
-  min = 2;
+  sliderValue = 0;
 
-  max = 9;
+  textValue = '';
 
-  step = 0.05;
+  step = 0.1;
 
-  sliderX = this.min;
+  ignoreNextSliderChange = false;
 
   chartOptions = new DefaultChartOptions();
 
@@ -60,64 +84,196 @@ export default class UniformXDependentDisplay extends Vue implements IParameterD
 
   temp = false;
 
-  get exampleData(): DefaultChartData {
-    const x = [this.min, 4, 5, 6, this.max];
-    const ymin = [1, 4, 0.6, 0.8, 1];
-    const ymax = [1, 8, 10, 7, 5];
-    const colorProvider = new CycleColorProvider();
-    const mins = x.map((v, i) => {
-      return new ChartPoint2D(v, ymin[i]);
-    });
-    mins.push(new ChartPoint2D(x[x.length - 1], ymax[ymax.length - 1]));
-    const maxs = x.map((v, i) => {
-      return new ChartPoint2D(v, ymax[i]);
-    });
-    const minScatter = new ScatterChartDataset(mins, 'Example Mins', colorProvider);
-    const maxScatter = new ScatterChartDataset(maxs, 'Example Maxs', colorProvider);
+  xValues: number[] = [];
 
-    const min = this.interpolate(ymin, x, this.sliderX);
-    const max = this.interpolate(ymax, x, this.sliderX);
-    // let uGen = new UniformDistribution(min, max);
-    // let uGen = Uniform();
-    // don't we need a number generated between min and max?
-    // just using half way for now
-    const currentValue = (min + max) / 2;
-    const currentChartPoint = new ChartPoint2D(this.sliderX, currentValue);
-    const currentScatterPoint = new ScatterChartDataset([currentChartPoint], 'Current', colorProvider);
-    return new DefaultChartData([maxScatter, minScatter, currentScatterPoint]);
+  yMaxValues: number[] = [];
+
+  yMinValues: number[] = [];
+
+  dependentVariables?: string[] = [];
+
+  get variableSets(): VariableSet[] {
+    const uniqueVariables = [...new Set(this.dependentVariables)];
+    const sets = uniqueVariables.map((name) => {
+      const indices: number[] = [];
+      const points: number[] = [];
+      return {
+        name,
+        indices,
+        points,
+        value: undefined,
+      };
+    });
+
+    // group points and indices with sets
+    this.xValues.forEach((x, i) => {
+      const currentSet = this.dependentVariables ? this.dependentVariables[i] : undefined;
+      if (currentSet !== undefined) {
+        const setIndex = sets.map((set) => set.name).indexOf(currentSet);
+        sets[setIndex].points.push(x);
+        sets[setIndex].indices.push(i);
+      }
+    });
+
+    return sets;
   }
 
-  /* eslint class-methods-use-this: ["error", { "exceptMethods": ["interpolate"] }] */
+  get selectedSet(): VariableSet {
+    return this.variableSets.filter((set) => set.name === this.selectedSetName)[0];
+  }
+
+  selectedSetName = '';
+
+  get min(): number {
+    return this.selectedSet.points.sort((a, b) => a - b)[0];
+  }
+
+  get max(): number {
+    return this.selectedSet.points.sort((a, b) => b - a)[0];
+  }
+
+  get displayChart(): boolean {
+    return this.selectedSet && this.yMinValues.length > 0 && this.yMaxValues.length > 0;
+  }
+
+  get chartData(): DefaultChartData {
+    const dataSets: ScatterChartDataset[] = [];
+    const colorProvider = new CycleColorProvider();
+
+    const selectedXValues = this.selectedSet.points.sort((a, b) => a - b);
+
+    const selectedYMins = this.yMinValues.filter((y, i) => this.selectedSet.indices.includes(i));
+    const selectedYMaxs = this.yMaxValues.filter((y, i) => this.selectedSet.indices.includes(i));
+
+    const mins: ChartPoint2D[] = selectedXValues.map((x, i) => new ChartPoint2D(x, selectedYMins[i]));
+    const maxs: ChartPoint2D[] = selectedXValues.map((x, i) => new ChartPoint2D(x, selectedYMaxs[i]));
+
+    if (mins.length) {
+      const minScatter = new ScatterChartDataset(mins, 'Min', colorProvider);
+      dataSets.push(minScatter);
+    }
+
+    if (maxs.length) {
+      const maxScatter = new ScatterChartDataset(maxs, 'Max', colorProvider);
+      dataSets.push(maxScatter);
+    }
+
+    const min = this.interpolate(selectedYMins, selectedXValues, this.sliderValue);
+    const max = this.interpolate(selectedYMaxs, selectedXValues, this.sliderValue);
+
+    const currentMin = new ChartPoint2D(this.sliderValue, min);
+    const currentMax = new ChartPoint2D(this.sliderValue, max);
+
+    const current = new ScatterChartDataset([currentMin, currentMax], 'Current', colorProvider);
+    dataSets.push(current);
+
+    return new DefaultChartData(dataSets);
+  }
+
+  @Watch('sliderValue')
+  onSliderValueChanged(newValue: number): void {
+    if (!this.ignoreNextSliderChange) {
+      this.textValue = newValue.toString();
+      this.selectedSet.value = newValue;
+    } else {
+      this.ignoreNextSliderChange = false;
+    }
+  }
+
+  @Watch('parameterValue')
+  onParameterChanged(): void {
+    this.setValues();
+  }
+
+  @Watch('selectedSet')
+  onSelectedSetChanged(): void {
+    if (this.selectedSet.value !== undefined) {
+      this.sliderValue = this.selectedSet.value;
+    } else {
+      this.sliderValue = this.min;
+    }
+  }
+
+  // eslint-disable-next-line class-methods-use-this
   interpolate(Y: number[], X: number[], val: number): number {
     let minIndex = 0;
     let maxIndex = X.length - 1;
     // there has got to be a better way to loop through X
-    for (let i = 0; i < X.length; ) {
+    for (let i = 0; i < X.length; i += 1) {
       if (X[i] <= val) {
         minIndex = i;
       } else if (X[i] >= val) {
         maxIndex = i;
         break;
       }
-      i += 1; // eslint doesn't like the i++
     }
     const slope = (Y[maxIndex] - Y[minIndex]) / (X[maxIndex] - X[minIndex]);
     const yDelta = slope * (val - X[minIndex]);
     return yDelta + Y[minIndex];
   }
 
-  toggleLoading(): void {
-    this.loading = !this.loading;
+  onTextEnterPressed(event: KeyboardEvent): void {
+    if (event.keyCode === Key.Enter) {
+      this.updateOnTextChange();
+    }
   }
 
-  toggleTemp(): void {
-    this.temp = !this.temp;
+  updateOnTextChange(): void {
+    const value = Number(this.textValue);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const castComponent = this.$refs.value as any;
+    if (this.textValue === '') {
+      this.selectedSet.value = undefined;
+    } else if (value === this.sliderValue) {
+      this.selectedSet.value = value;
+    } else if (!this.parameterValue.isSet && !castComponent.validate(true)) {
+      this.textValue = '';
+    } else if (castComponent.validate && castComponent.validate(true)) {
+      this.sliderValue = value;
+      this.selectedSet.value = value;
+    } else {
+      this.textValue = this.sliderValue.toString();
+    }
+  }
+
+  validationRules(value: string): boolean | string {
+    const num = Number(value);
+    if (Number.isNaN(num)) {
+      return 'Value must be number!';
+    }
+    if (num > this.max) {
+      return `Value must be less than or equal to ${this.max}`;
+    }
+    if (num < this.min) {
+      return `Value must be greater than or equal to ${this.min}`;
+    }
+    return true;
   }
 
   vuetifyColorProps(): unknown {
     return {
       '--primary-color': this.$vuetify.theme.currentTheme.primary,
     };
+  }
+
+  setValues(): void {
+    this.step = this.parameterValue.metaData.step ?? Math.max((this.max - this.min) / 1000, 0.1);
+
+    this.ignoreNextSliderChange = true;
+    // this.sliderValue = this.min;
+
+    // this.textValue = this.sliderValue.toString();
+
+    this.xValues = this.parameterValue.xValues ?? [];
+    this.yMinValues = this.parameterValue.yMinimumValues ?? [];
+    this.yMaxValues = this.parameterValue.yMaximumValues ?? [];
+    this.dependentVariables = this.parameterValue.dependentVariable;
+
+    this.selectedSetName = this.variableSets[0].name;
+  }
+
+  created(): void {
+    this.setValues();
   }
 }
 </script>
