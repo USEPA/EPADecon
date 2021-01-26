bd2c263f3cb3a55be16ac7107e2f9d7e980b5e10
emily <PERAZA@battelle.org>
2021-01-26 13:34:17 -0500

    Added the 'other' parameters

---
+++ Battelle.EPA.WideAreaDecon.InterfaceData/Models/Parameter/OtherParameters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Battelle.EPA.WideAreaDecon.InterfaceData.Models.Parameter
+{
+    public class OtherParameters
+    {
+        public double personnelPerRentalCar;
+
+        public OtherParameters(ParameterFilter[] parameters)
+        {
+
+        }
+    }
+}
