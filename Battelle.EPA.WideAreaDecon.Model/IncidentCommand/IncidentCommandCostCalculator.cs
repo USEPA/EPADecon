45795ce716756be4744956429f8531ebbad174d4
emily <PERAZA@battelle.org>
2021-01-27 14:31:04 -0500

    Fixed formatting

--- Battelle.EPA.WideAreaDecon.Model/IncidentCommand/IncidentCommandCostCalculator.cs
+++ Battelle.EPA.WideAreaDecon.Model/IncidentCommand/IncidentCommandCostCalculator.cs
@@ -1,4 +1,4 @@
-﻿using Battelle.EPA.WideAreaDecon.Model.IncidentCommand;
+using Battelle.EPA.WideAreaDecon.Model.IncidentCommand;
 using System.Collections.Generic;
 using Battelle.EPA.WideAreaDecon.Model.Services;
 using Battelle.EPA.WideAreaDecon.InterfaceData;
@@ -10,21 +10,13 @@ namespace Battelle.EPA.WideAreaDecon.Model.IncidentCommand
     {
         private LaborCostCalculator Calculator_labor { get; set; }
         private SuppliesCostCalculator Calculator_supplies { get; set; }
-
         private CharacterizationSampling.LaborCostCalculator Calculator_laborCs { get; set; }
-
         private CharacterizationSampling.PhaseLagCalculator Calculator_phaseLag { get; set; }
-
         private CharacterizationSampling.SuppliesCostCalculator Calculator_suppliesCs { get; set; }
-
         private SourceReduction.LaborCostCalculator Calculator_laborSr { get; set; }
-
         private SourceReduction.WorkDaysCalculator Calculator_workDaysSr { get; set; }
-
         private Decontamination.LaborCostCalculator Calculator_laborDc { get; set; }
-
         private Decontamination.EfficacyCalculator Calculator_efficacy { get; set; }
-
         private Decontamination.WorkDaysCalculator Calculator_workDaysDc { get; set; }
 
         public double CalculateCost(double _numberTeams, double surfaceAreaToBeSourceReduced, double personnelRoundTripDays,
