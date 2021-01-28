c905fe6f6085b3d7070d5a521024db4f0fb375ed
Daniel Sharp <sharpd@battelle.org>
2021-01-28 14:27:39 -0500

    updated run scenario method

--- Battelle.EPA.WideAreaDecon.Client/src/store/jobs/actions.ts
+++ Battelle.EPA.WideAreaDecon.Client/src/store/jobs/actions.ts
@@ -1,10 +1,16 @@
 import { ActionTree } from 'vuex';
 import IRootState from '@/interfaces/store/IRootState';
-import IJobProvider from '@/interfaces/providers/IJobProvider';
+import IRunScenarioPayload from '@/interfaces/store/jobs/IRunScenarioPayload';
 
 const jobActions: ActionTree<IRootState, IRootState> = {
-  runScenario: ({ state }, jobProvider: IJobProvider) => {
-    const job = jobProvider.createJobRequest(state.scenarioDefinition, state.scenarioParameters);
+  runScenario: ({ state }, { jobProvider, numberRealizations, seed1, seed2 }: IRunScenarioPayload) => {
+    const job = jobProvider.createJobRequest(
+      state.scenarioDefinition,
+      state.scenarioParameters,
+      numberRealizations,
+      seed1,
+      seed2,
+    );
     jobProvider.postJobRequest(job);
   },
 };
