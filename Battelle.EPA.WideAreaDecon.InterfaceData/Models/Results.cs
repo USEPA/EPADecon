07e23e337fb4e35d990aeef771a478c54e4f4d9f
emily <PERAZA@battelle.org>
2021-02-17 14:33:06 -0500

    Added results class

---
+++ Battelle.EPA.WideAreaDecon.InterfaceData/Models/Results.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Battelle.EPA.WideAreaDecon.InterfaceData.Enumeration.Parameter;
+
+namespace Battelle.EPA.WideAreaDecon.InterfaceData.Models
+{
+    public class Results
+    {
+        public Dictionary<PhaseCategory, double> workDays { get; set; }
+        public Dictionary<PhaseCategory, double> onSiteDays { get; set; }
+        public Dictionary<PhaseCategory, double> phaseCost { get; set; }
+        public double totalCost { get; set; }
+    }
+}
