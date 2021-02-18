0c714a72cf0baf2f54d59a3a33c9b6f5c67f625f
emily <PERAZA@battelle.org>
2021-02-18 16:06:29 -0500

    Moved results class and implemented different variables

---
+++ Battelle.EPA.WideAreaDecon.InterfaceData/Models/Results/Results.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Battelle.EPA.WideAreaDecon.InterfaceData.Models.Results
+{
+    public class Results
+    {
+        public GenericPhaseResults characterizationSamplingResults { get; set; }
+        public GenericPhaseResults sourceReductionResults { get; set; }
+        public GenericPhaseResults decontaminationResults { get; set; }
+        public IncidentCommandResults incidentCommandResults { get; set; }
+        public OtherResults otherResults { get; set; }
+        public GeneralResults generalResults { get; set; }
+    }
+}
