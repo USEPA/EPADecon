06d98a1f83cc1f2c5bd171bd851a14fc277df293
emily <PERAZA@battelle.org>
2021-02-18 16:06:49 -0500

    Broke results out into separate classes

---
+++ Battelle.EPA.WideAreaDecon.InterfaceData/Models/Results/GeneralResults.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Battelle.EPA.WideAreaDecon.InterfaceData.Models.Results
+{
+    public class GeneralResults
+    {
+        public double totalCost { get; set; }
+        public double areaContaminated { get; set; }
+    }
+}
