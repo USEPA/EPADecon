d9d6d880a0dac61eaee7c158ce3e66d51676922e
emily <PERAZA@battelle.org>
2021-02-23 14:45:26 -0500

    Template for job manager unit test

---
+++ Battelle.EPA.WideAreaDecon.API.Tests/Services/JobManagerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Battelle.EPA.WideAreaDecon.API.Services;
+
+namespace Battelle.EPA.WideAreaDecon.API.Tests.Services
+{
+    public class JobManagerTest
+    {
+        private JobManager Calculator { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void ConvertAndExecuteJob()
+        {
+
+        }
+    }
+}
