6c5eb67f487dbc1d03c71ceea0c86288935732c2
emily <PERAZA@battelle.org>
2021-02-23 18:33:20 -0500

    Fixed unit test that was commented out

--- Battelle.EPA.WideAreaDecon.Model.Tests/Decontamination/WorkDaysCalculatorTests.cs
+++ Battelle.EPA.WideAreaDecon.Model.Tests/Decontamination/WorkDaysCalculatorTests.cs
@@ -6,20 +6,6 @@ using Battelle.EPA.WideAreaDecon.InterfaceData.Enumeration.Parameter;
 
 namespace Battelle.EPA.WideAreaDecon.Model.Tests.Decontamination
 {
-    internal class MockEfficacyCalculator : IEfficacyCalculator
-    {
-        public Dictionary<SurfaceType, double> CalculateEfficacy(Dictionary<SurfaceType, double> _initialSporeLoading)
-        {
-            var dbl = new Dictionary<SurfaceType, double>();
-            foreach (SurfaceType surface in Enum.GetValues(typeof(SurfaceType)))
-            {
-                dbl.Add(surface, 0.9);
-            }
-
-            return dbl;
-        }
-    }
-
     public class WorkDaysCalculatorTests
     {
         private WorkDaysCalculator Calculator { get; set; }
@@ -27,25 +13,30 @@ namespace Battelle.EPA.WideAreaDecon.Model.Tests.Decontamination
         [SetUp]
         public void Setup()
         {
-            var applicationMethods = new Dictionary<SurfaceType, ApplicationMethod>()
+            var desiredSporeThreshold = 0.0;
+            var applicationMethods = new Dictionary<SurfaceType, ApplicationMethod>();
+            var initialSporeLoading = new Dictionary<SurfaceType, double>();
+            var treatmentDaysPerAm = new Dictionary<ApplicationMethod, double>();
+            var efficacyValues = new Dictionary<SurfaceType, double>();
+
+            foreach (SurfaceType surface in Enum.GetValues(typeof(SurfaceType)))
             {
-                { SurfaceType.IndoorInterior, ApplicationMethod.FoamSpray}
-            };
-            var initialSporeLoading = new Dictionary<SurfaceType, double>()
+                applicationMethods.Add(surface, ApplicationMethod.Fogging);
+                initialSporeLoading.Add(surface, 200.0);
+                efficacyValues.Add(surface, 50.0);
+            }
+
+            foreach (ApplicationMethod method in Enum.GetValues(typeof(ApplicationMethod)))
             {
-                { SurfaceType.IndoorInterior, 1.1}
-            };
-            var desiredSporeThreshold = 1.0;
-            var treatmentDaysPerAm = new Dictionary<ApplicationMethod, double>()
-            {
-                { ApplicationMethod.FoamSpray, 3.0}
-            };
+                treatmentDaysPerAm.Add(method, 3.0);
+            }
+
             Calculator = new WorkDaysCalculator(
                 applicationMethods,
                 initialSporeLoading,
                 desiredSporeThreshold,
                 treatmentDaysPerAm,
-                new MockEfficacyCalculator()
+                new EfficacyCalculator(efficacyValues)
             );
         }
 
@@ -53,7 +44,7 @@ namespace Battelle.EPA.WideAreaDecon.Model.Tests.Decontamination
         public void CalculateCost()
         {
 
-            //Assert.AreEqual(3.0, Calculator.CalculateWorkDays(), 1e-6, "Incorrect cost calculated"); TODO: Fix
+            Assert.AreEqual(12.0, Calculator.CalculateWorkDays(), 1e-6, "Incorrect cost calculated");
 
         }
     }
