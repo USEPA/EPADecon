cbb8dcd21e5a0c96befa9985a9f3634d0cc9c852
Daniel Sharp <sharpd@battelle.org>
2021-03-18 15:02:55 -0400

    changed result export format, missing averages

--- Battelle.EPA.WideAreaDecon.Client/src/implementations/providers/JobProvider.ts
+++ Battelle.EPA.WideAreaDecon.Client/src/implementations/providers/JobProvider.ts
@@ -2,13 +2,16 @@ import { injectable } from 'inversify';
 import JobStatus from '@/enums/jobs/jobStatus';
 import IJobProvider from '@/interfaces/providers/IJobProvider';
 import Axios from 'axios';
+import XLSX from 'xlsx';
+import IJobResultRealization from '@/interfaces/jobs/results/IJobResultRealization';
+import IPhaseResult from '@/interfaces/jobs/results/IPhaseResult';
 import { serialize } from 'typescript-json-serializer';
 import JobRequest from '../jobs/JobRequest';
 import ParameterWrapperList from '../parameter/ParameterWrapperList';
 
 @injectable()
 export default class JobProvider implements IJobProvider {
-  // eslint-disable-next-line class-methods-use-this
+  /* eslint-disable class-methods-use-this */
   createJobRequest(
     scenarioDefinition: ParameterWrapperList,
     scenarioParameters: ParameterWrapperList,
@@ -20,29 +23,25 @@ export default class JobProvider implements IJobProvider {
     return job;
   }
 
-  // eslint-disable-next-line class-methods-use-this
   async postJobRequest(job: JobRequest): Promise<string> {
     const serializedJob = serialize(job);
-    return Axios.post<JobRequest>('/api/JobRequest', serializedJob)
-      .then((response) => `${response.data}`)
-      .catch(() => '');
+    return Axios.post<string>('/api/JobRequest', serializedJob).then((response) => response.data);
   }
 
-  // eslint-disable-next-line class-methods-use-this
   async getJob(id: string): Promise<JobRequest> {
     return Axios.get<JobRequest>(`/api/JobRequest`, { params: { id } }).then((response) => response.data);
   }
 
-  // eslint-disable-next-line class-methods-use-this
   exportJobResults(job: JobRequest): void {
     const { results } = job;
     if (!results) {
       return;
     }
 
-    const categoryHeaders = [
-      '',
-      '',
+    const wb = XLSX.utils.book_new();
+
+    // Headers for each phase
+    const phaseHeaders = [
       'Characterization Sampling',
       '',
       '',
@@ -56,51 +55,82 @@ export default class JobProvider implements IJobProvider {
       '',
       'Other',
       'General',
-      '',
     ];
 
-    const colHeaders = ['', '', ...results.flatMap((r) => Object.values(r.Outdoor).flatMap((ps) => Object.keys(ps)))];
-
-    const indoorResults = results.map((r) =>
-      Object.entries(r.Indoor)
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        .map(([b, res]) => ['', b, ...Object.values(res).flatMap((ps: any) => Object.values(ps))])
-        .join('\n'),
+    // Get headers for each result
+    const resultHeaders = Object.values(results[0].Outdoor).flatMap((pr) =>
+      Object.keys(pr).map((p) => this.formatHeader(p)),
     );
-    const outdoorResults = [
-      '',
-      '',
-      ...results.flatMap((r) => Object.values(r.Outdoor).flatMap((ps) => Object.values(ps))),
-    ];
-    const underGroundResults = [
-      '',
-      '',
-      ...results.flatMap((r) => Object.values(r.Underground).flatMap((ps) => Object.values(ps))),
-    ];
 
-    const rows = [
-      categoryHeaders,
-      ['Indoor'],
-      ['', 'Building', ...colHeaders.filter((_, i) => i !== 0 && i !== 1)],
-      indoorResults,
-      [''], // Empty row
-      ['Outdoor'],
-      colHeaders,
-      outdoorResults,
-      [''], // Empty row
-      ['Underground'],
-      colHeaders,
-      underGroundResults,
+    // Headers for average results
+    const averageHeaders = resultHeaders.map((h) => `Average ${h}`);
+
+    // Build arrays for each location
+    const buildings = Object.keys(results[0].Indoor);
+    const indoor = buildings.map((b) =>
+      this.buildLocationResults(results, b, phaseHeaders, resultHeaders, averageHeaders, true),
+    );
+    const outdoor = this.buildLocationResults(results, 'Outdoor', phaseHeaders, resultHeaders, averageHeaders);
+    const underground = this.buildLocationResults(results, 'Underground', phaseHeaders, resultHeaders, averageHeaders);
+
+    const runData = [
+      ['Data exported on: ', new Date(Date.now()).toLocaleString()],
+      ['Number of realizations: ', job.numberRealizations],
     ];
 
-    const csvContent = `data:text/csv;charset=utf-8,${rows.map((e) => e.join(',')).join('\n')}`;
+    // Create worksheets from arrays
+    const dataWS = XLSX.utils.aoa_to_sheet(runData);
+    const indWS = indoor.map((aoa) => XLSX.utils.aoa_to_sheet(aoa));
+    const outWS = XLSX.utils.aoa_to_sheet(outdoor);
+    const undWS = XLSX.utils.aoa_to_sheet(underground);
 
-    const encodedUri = encodeURI(csvContent);
-    const link = document.createElement('a');
-    link.setAttribute('href', encodedUri);
-    link.setAttribute('download', 'job_results.csv');
-    document.body.appendChild(link); // Required for FF
+    // Add worksheets to workbook
+    XLSX.utils.book_append_sheet(wb, dataWS, 'Data');
+    indWS.forEach((WS, i) => XLSX.utils.book_append_sheet(wb, WS, `${buildings[i]} Building`));
+    XLSX.utils.book_append_sheet(wb, outWS, 'Outdoor');
+    XLSX.utils.book_append_sheet(wb, undWS, 'Underground');
 
-    link.click(); // download file
+    // Download workbook
+    XLSX.writeFile(wb, 'Results.xlsx');
+  }
+
+  private parseLocationRealizationResults(
+    results: IJobResultRealization[],
+    location: string,
+    isIndoor: boolean,
+  ): number[][] {
+    return results.map((r, i) => [
+      i + 1,
+      ...Object.values(isIndoor ? r.Indoor[location] : r[location]).flatMap((p: IPhaseResult) => Object.values(p)),
+    ]);
+  }
+
+  private formatHeader(header: string): string {
+    const withSpaces = header.split(/(?=[A-Z])/).join(' ');
+    return `${withSpaces.substring(0, 1).toUpperCase()}${withSpaces.substring(1, withSpaces.length)}`;
+  }
+
+  private buildLocationResults(
+    results: IJobResultRealization[],
+    location: string,
+    phaseHeaders: string[],
+    resultHeaders: string[],
+    averageHeaders: string[],
+    isIndoor = false,
+  ): (string | number)[][] {
+    return [
+      [`${isIndoor ? `${location} Building` : location} Results`],
+      [''], // empty row
+      ['', ...phaseHeaders],
+      ['', ...averageHeaders],
+      ['', ...[]],
+      [''],
+      [''],
+      ['Realization Results'],
+      [''],
+      ['', ...phaseHeaders],
+      ['Realization', ...resultHeaders],
+      ...this.parseLocationRealizationResults(results, location, isIndoor),
+    ];
   }
 }
