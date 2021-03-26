d3cd6774da6afa6c75aca597430684329388a778
Daniel Sharp <sharpd@battelle.org>
2021-03-26 08:25:38 -0400

    added result details, no chart yet

---
+++ Battelle.EPA.WideAreaDecon.Client/src/components/modals/results/ResultDetails.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="isVisible" max-width="800">
+      <v-card>
+        <v-card-title class="headline" v-text="`${title} Details`"></v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row> Histogram... </v-row>
+            <v-row>
+              <v-col>
+                <p class="text-body-2 mb-0">$123456777</p>
+                <p class="text-subtitle-1">Mean</p>
+              </v-col>
+              <v-col>
+                <p class="text-body-2 mb-0">$123456777</p>
+                <p class="text-subtitle-1">Maximum</p>
+              </v-col>
+              <v-col>
+                <p class="text-body-2 mb-0">$123456777</p>
+                <p class="text-subtitle-1">Minimum</p>
+              </v-col>
+              <v-col>
+                <p class="text-body-2 mb-0">$123456777</p>
+                <p class="text-subtitle-1">Standard Deviation</p>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn outlined color="primary darken-1" text @click="isVisible = false"> Close </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script lang="ts">
+import { Component, Prop, VModel, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ResultDetails extends Vue {
+  @Prop() title!: string;
+
+  @VModel({ default: () => false }) isVisible!: boolean;
+}
+</script>
+
+<style></style>
