563d9a6387db71fe2f498106b31bde7e3611a5ab
Daniel Sharp <sharpd@battelle.org>
2021-04-19 15:35:02 -0400

    add histogram to result details

--- Battelle.EPA.WideAreaDecon.Client/src/components/modals/results/ResultDetails.vue
+++ Battelle.EPA.WideAreaDecon.Client/src/components/modals/results/ResultDetails.vue
@@ -5,22 +5,26 @@
         <v-card-title class="headline" v-text="`${title} Details`"></v-card-title>
         <v-card-text>
           <v-container>
-            <v-row> Histogram... </v-row>
+            <v-row>
+              <v-col style="height: 400px">
+                <chart-js-wrapper type="bar" :data="chartData" :options="options" :plugins="[]" :key="title" />
+              </v-col>
+            </v-row>
             <v-row>
               <v-col>
-                <p class="text-body-2 mb-0">{{ details.mean }}</p>
+                <p class="text-body-2 mb-0">{{ resultProvider.formatNumber(details.mean) }}</p>
                 <p class="text-subtitle-1">Mean</p>
               </v-col>
               <v-col>
-                <p class="text-body-2 mb-0">{{ details.maximum }}</p>
+                <p class="text-body-2 mb-0">{{ resultProvider.formatNumber(details.maximum) }}</p>
                 <p class="text-subtitle-1">Maximum</p>
               </v-col>
               <v-col>
-                <p class="text-body-2 mb-0">{{ details.minimum }}</p>
+                <p class="text-body-2 mb-0">{{ resultProvider.formatNumber(details.minimum) }}</p>
                 <p class="text-subtitle-1">Minimum</p>
               </v-col>
               <v-col>
-                <p class="text-body-2 mb-0">{{ details.stdDev }}</p>
+                <p class="text-body-2 mb-0">{{ resultProvider.formatNumber(details.stdDev) }}</p>
                 <p class="text-subtitle-1">Standard Deviation</p>
               </v-col>
             </v-row>
@@ -36,16 +40,129 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, VModel, Vue } from 'vue-property-decorator';
+import { Component, Prop, VModel, Vue, Watch } from 'vue-property-decorator';
+import {
+  ChartJsWrapper,
+  CycleColorProvider,
+  DefaultChartData,
+  DefaultChartOptions,
+} from 'battelle-common-vue-charting/src';
+import { ChartData } from 'chart.js';
 import IResultDetails from '@/interfaces/jobs/results/IResultDetails';
+import container from '@/dependencyInjection/config';
+import IJobResultProvider from '@/interfaces/providers/IJobResultProvider';
+import TYPES from '@/dependencyInjection/types';
+import { range } from 'lodash';
 
-@Component
+@Component({ components: { ChartJsWrapper } })
 export default class ResultDetails extends Vue {
   @Prop() title!: string;
 
   @Prop() details!: IResultDetails;
 
   @VModel({ default: () => false }) isVisible!: boolean;
+
+  private resultProvider = container.get<IJobResultProvider>(TYPES.JobResultProvider);
+
+  options = new DefaultChartOptions();
+
+  numberOfBins = 5;
+
+  chartTicks: number[] = [];
+
+  get chartData(): ChartData {
+    if (!this.details) {
+      return new DefaultChartData([]);
+    }
+
+    const color = new CycleColorProvider().getNextColor();
+
+    // create bins
+    const step = (this.details.maximum - this.details.minimum) / this.numberOfBins;
+    const bins = step ? range(this.details.minimum, this.details.maximum, step) : [this.details.minimum];
+
+    // put values into bins
+    const binVals: number[] = [];
+    this.details.values.forEach((v) => {
+      let index = bins.findIndex((b, i) => v >= b && v < bins[i + 1]);
+      if (index === -1) {
+        // value falls into last bin
+        index = bins.length - 1;
+      }
+
+      if (binVals[index] !== undefined) {
+        binVals[index] += 1;
+      } else {
+        binVals[index] = 1;
+      }
+    });
+
+    return {
+      labels: bins,
+      datasets: [
+        {
+          data: binVals,
+          backgroundColor: color,
+          barPercentage: 1,
+          categoryPercentage: 1,
+          borderWidth: 1,
+        },
+      ],
+    };
+  }
+
+  @Watch('details')
+  onDetailsChanged(): void {
+    this.initializeChart();
+  }
+
+  @Watch('chartData')
+  onChartDataChagned(newValue: ChartData): void {
+    this.chartTicks = newValue.labels as number[];
+  }
+
+  initializeChart(): void {
+    this.options.legend.display = false;
+
+    this.options.tooltips.enabled = true;
+    this.options.tooltips.callbacks = {
+      title: ([item]) => {
+        const index = item.index ?? 0;
+        const upper = this.resultProvider.formatNumber(this.chartTicks[index + 1] ?? this.details.maximum);
+        const lower = this.resultProvider.formatNumber(this.chartTicks[index]);
+        return `${lower} - ${upper}`;
+      },
+    };
+
+    this.options.scales = {
+      xAxes: [
+        {
+          scaleLabel: {
+            display: true,
+            labelString: this.title,
+          },
+          ticks: {
+            callback: (value: number) => this.resultProvider.formatNumber(value),
+          },
+        },
+      ],
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+          scaleLabel: {
+            display: true,
+            labelString: 'Number of Realizations',
+          },
+        },
+      ],
+    };
+  }
+
+  created(): void {
+    this.initializeChart();
+  }
 }
 </script>
 
