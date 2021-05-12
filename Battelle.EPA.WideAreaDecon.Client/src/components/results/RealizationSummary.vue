e728cb12b24f4b5af1eaa9ad0b5172de1d4bc212
Daniel Sharp <sharpd@battelle.org>
2021-05-12 17:30:03 -0400

    comparison table layout changes, still needs work

--- Battelle.EPA.WideAreaDecon.Client/src/components/results/RealizationSummary.vue
+++ Battelle.EPA.WideAreaDecon.Client/src/components/results/RealizationSummary.vue
@@ -20,127 +20,140 @@
       </v-col>
     </v-row>
 
-    <!-- TODO MAKE OWN COMPONENT -->
+    <!-- TODO: MAKE OWN COMPONENT -->
     <v-row>
       <v-col>
         <v-card>
-          <v-row dense>
-            <v-col cols="auto" class="mr-auto">
-              <v-card-title v-text="'Realization Comparison'" />
-            </v-col>
+          <v-container>
+            <v-row style="margin-bottom: 7px">
+              <v-col cols="auto">
+                <v-card-title v-text="'Realization Comparison'" />
+              </v-col>
 
-            <v-col style="margin-top: 7px" cols="2">
-              <v-select
-                label="Building"
-                :items="['All', ...locations]"
-                v-model="selectedLocation"
-                outlined
-                hide-details="auto"
-              ></v-select>
-            </v-col>
+              <v-col class="ml-auto" cols="3" style="margin-top: 7px">
+                <v-select
+                  label="Building"
+                  :items="['All', ...locations]"
+                  v-model="selectedLocation"
+                  outlined
+                  hide-details="auto"
+                ></v-select>
+              </v-col>
 
-            <v-col style="margin-top: 7px" cols="2" class="d-inline-flex">
-              <v-text-field
-                label="Run Number"
-                v-model.number="runNumber"
-                type="number"
-                :rules="[validationRulesRunNumber]"
-                hide-details="auto"
-                outlined
-              ></v-text-field>
+              <v-col cols="2" style="margin-top: 7px">
+                <v-text-field
+                  label="Run Number"
+                  v-model.number="runNumber"
+                  type="number"
+                  :rules="[validationRulesRunNumber]"
+                  hide-details="auto"
+                  outlined
+                ></v-text-field>
+              </v-col>
 
-              <v-btn
-                height="45"
-                color="secondary"
-                @click="addRunToTable"
-                :disabled="validationRulesRunNumber(runNumber) !== true"
-              >
-                View
-              </v-btn>
-            </v-col>
-          </v-row>
+              <v-col cols="1" class="align-self-center">
+                <v-btn
+                  height="45"
+                  color="secondary"
+                  @click="addRunToTable"
+                  :disabled="validationRulesRunNumber(runNumber) !== true"
+                >
+                  View
+                </v-btn>
+              </v-col>
+            </v-row>
 
-          <v-divider color="grey"></v-divider>
+            <v-divider color="grey"></v-divider>
 
-          <v-simple-table v-if="displayedRunNumbers.length" dense class="overflow-x-auto">
-            <template v-if="selectedLocation !== 'All'" v-slot:default>
-              <thead>
-                <tr>
-                  <th></th>
-                  <th class="text-right text-body-1" v-for="runNumber in displayedRunNumbers" :key="runNumber">
-                    Run {{ runNumber }}
-                    <v-icon class="ml-1" small @click="removeRunFromTable(runNumber)">mdi-close-circle</v-icon>
+            <v-simple-table v-if="displayedRunNumbers.length" dense class="overflow-x-auto">
+              <template v-if="selectedLocation !== 'All'" v-slot:default>
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th class="text-body-1" v-for="runNumber in displayedRunNumbers" :key="runNumber">
+                      Run {{ runNumber }}
+                      <v-icon class="ml-1" small @click="removeRunFromTable(runNumber)">mdi-close-circle</v-icon>
 
-                    <v-btn class="d-block ml-auto" x-small>Summary</v-btn>
-                  </th>
-                </tr>
-              </thead>
+                      <!-- <v-btn class="d-block ml-auto" x-small>Summary</v-btn> -->
+                    </th>
+                  </tr>
+                </thead>
 
-              <tbody v-for="(phaseResult, phaseName) in results[0].Outdoor" :key="phaseName">
-                <tr>
-                  <td class="text-subtitle-1 font-weight-medium">
-                    {{ resultProvider.convertCamelToTitleCase(phaseName) }}
-                  </td>
-                  <td :colspan="displayedRunNumbers.length"></td>
-                </tr>
+                <tbody v-for="(phaseResult, phaseName) in results[0].Outdoor" :key="phaseName">
+                  <tr>
+                    <td class="text-subtitle-1 font-weight-medium">
+                      {{ resultProvider.convertCamelToTitleCase(phaseName) }}
+                    </td>
+                    <td :colspan="displayedRunNumbers.length"></td>
+                  </tr>
 
-                <tr v-for="(_, result) in phaseResult" :key="result">
-                  <td class="pl-8">{{ resultProvider.convertCamelToTitleCase(result) }}</td>
+                  <tr v-for="(_, result) in phaseResult" :key="result">
+                    <td class="pl-8">{{ resultProvider.convertCamelToTitleCase(result) }}</td>
 
-                  <td class="text-right" v-for="runNumber in displayedRunNumbers" :key="runNumber">
-                    {{ getLocationResults(runNumber)[phaseName][result] }}
-                  </td>
-                </tr>
-              </tbody>
-            </template>
+                    <td v-for="runNumber in displayedRunNumbers" :key="runNumber">
+                      {{ getLocationResults(runNumber)[phaseName][result] }}
+                    </td>
+                  </tr>
+                </tbody>
+              </template>
 
-            <template v-else v-slot:default>
-              <thead>
-                <tr>
-                  <th></th>
-                  <th
-                    class="text-center text-body-1"
-                    :colspan="locations.length"
-                    v-for="runNumber in displayedRunNumbers"
-                    :key="runNumber"
-                  >
-                    Run {{ runNumber }}
-                    <v-icon class="ml-1" small @click="removeRunFromTable(runNumber)">mdi-close-circle</v-icon>
-                  </th>
-                </tr>
+              <template v-else v-slot:default>
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th
+                      class="text-center text-body-1 border-right"
+                      :colspan="locations.length"
+                      v-for="runNumber in displayedRunNumbers"
+                      :key="runNumber"
+                    >
+                      Run {{ runNumber }}
+                      <v-icon class="ml-1" small @click="removeRunFromTable(runNumber)">mdi-close-circle</v-icon>
+                    </th>
+                  </tr>
 
-                <tr>
-                  <th></th>
-                  <th
-                    class="text-right text-body-1"
-                    v-for="(location, i) in tableLocations"
-                    :key="`${location} - ${i}`"
-                  >
-                    {{ location }}
-                  </th>
-                </tr>
-              </thead>
+                  <tr>
+                    <th></th>
+                    <th
+                      :class="`text-body-1 ${getCellClass(i + 1)}`"
+                      v-for="(location, i) in tableLocations"
+                      :key="`${location} - ${i}`"
+                    >
+                      {{ location }}
+                    </th>
+                  </tr>
+                </thead>
 
-              <tbody v-for="(phaseResult, phaseName) in results[0].Outdoor" :key="phaseName">
-                <tr>
-                  <td class="text-subtitle-1 font-weight-medium">
-                    {{ resultProvider.convertCamelToTitleCase(phaseName) }}
-                  </td>
-                  <td :colspan="locations.length * displayedRunNumbers.length"></td>
-                </tr>
+                <tbody v-for="(phaseResult, phaseName) in results[0].Outdoor" :key="phaseName">
+                  <tr>
+                    <td class="text-subtitle-1 font-weight-medium">
+                      {{ resultProvider.convertCamelToTitleCase(phaseName) }}
+                    </td>
 
-                <tr v-for="(_, result) in phaseResult" :key="result">
-                  <td>{{ resultProvider.convertCamelToTitleCase(result) }}</td>
+                    <td
+                      :class="getCellClass(i)"
+                      v-for="i in locations.length * displayedRunNumbers.length"
+                      :key="i"
+                    ></td>
+                  </tr>
 
-                  <td class="text-right" v-for="(location, i) in tableLocations" :key="`${location} - ${i}`">
-                    {{ getLocationResults(calculateRunNumber(i), location)[phaseName][result] }}
-                  </td>
-                </tr>
-              </tbody>
-            </template>
-          </v-simple-table>
+                  <tr v-for="(_, result) in phaseResult" :key="result">
+                    <td>{{ resultProvider.convertCamelToTitleCase(result) }}</td>
 
-          <v-card-text v-else>Please select at least one realization to display a summary for</v-card-text>
+                    <td
+                      :class="getCellClass(i + 1)"
+                      v-for="(location, i) in tableLocations"
+                      :key="`${location} - ${i}`"
+                    >
+                      {{ getLocationResults(calculateRunNumber(i), location)[phaseName][result] }}
+                    </td>
+                  </tr>
+                </tbody>
+              </template>
+            </v-simple-table>
+
+            <v-card-text v-else>Please select at least one realization to display a summary for</v-card-text>
+          </v-container>
         </v-card>
       </v-col>
     </v-row>
@@ -164,7 +177,6 @@ import {
   DefaultChartData,
   ScatterChartDataset,
 } from 'battelle-common-vue-charting/src';
-// import mockResults from '@/dataMocks/mockResults';
 import PhaseResult from '@/enums/jobs/results/phaseResult';
 import IResultDetails from '@/interfaces/jobs/results/IResultDetails';
 import ChartOptions from '@/components/modals/results/ChartOptions.vue';
@@ -216,8 +228,8 @@ export default class RealizationSummary extends Vue {
     return Math.floor((index + length) / length);
   }
 
-  createHistogram({ values, minimum, maximum }: IResultDetails, label: PhaseResult | null): ChartData {
-    const xLabel = this.resultProvider.convertCamelToTitleCase(label as string);
+  // eslint-disable-next-line class-methods-use-this
+  createHistogram({ values, minimum, maximum }: IResultDetails): ChartData {
     const color = new CycleColorProvider().getNextColor();
 
     const numberOfBins = 10;
@@ -246,7 +258,7 @@ export default class RealizationSummary extends Vue {
       labels: bins,
       datasets: [
         {
-          // label: 'Number of Realizations',
+          label: 'Number of Realizations',
           data: binVals,
           backgroundColor: color,
           barPercentage: 1,
@@ -306,7 +318,7 @@ export default class RealizationSummary extends Vue {
     if (xDetails && !yDetails) {
       // histogram
       this.chartType = 'bar';
-      this.chartData = this.createHistogram(xDetails, x);
+      this.chartData = this.createHistogram(xDetails);
     } else if (!xDetails && yDetails) {
       // pie chart
       this.chartType = 'pie';
@@ -320,6 +332,10 @@ export default class RealizationSummary extends Vue {
     }
 
     this.getOutputStatistics(x, y);
+  }
+
+  getCellClass(cellNumber: number): string {
+    return cellNumber && cellNumber % this.locations.length === 0 && cellNumber ? 'border-right' : '';
   }
 
   getLocationResults(runNumber: number, location?: string): IPhaseResultSet {
@@ -374,18 +390,28 @@ export default class RealizationSummary extends Vue {
 .v-data-table__wrapper > table {
   position: relative;
 
-  & > thead > tr > th:first-child {
-    width: 250px;
-  }
-
+  & > thead > tr > th:first-child,
   & > tbody > tr > td:first-child {
     left: 0;
     position: sticky;
-    background: #fff;
+    background: #cfd8dc;
+    z-index: 1;
 
-    &:hover {
-      background-color: green;
+    &:not(th):hover {
+      background-color: #b0bec5;
     }
   }
+
+  & > tbody > tr > td:last-child {
+    width: 100%;
+  }
+
+  & > thead > tr > th:not(:first-child) {
+    border-bottom: thin solid rgba(0, 0, 0, 0.12);
+  }
+}
+
+.border-right:not(:last-child) {
+  border-right: 2px solid rgba(0, 0, 0, 0.12);
 }
 </style>
