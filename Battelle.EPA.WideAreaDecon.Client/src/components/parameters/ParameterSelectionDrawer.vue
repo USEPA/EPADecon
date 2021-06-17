263d002690583e22f3ce4b67d7fb9b795a27ea49
Eli Detmers <detmers@battelle.org>
2021-06-17 18:19:48 -0400

    Addition of openlayers file, switch to activate file
    beginning of geospatial implementation

--- Battelle.EPA.WideAreaDecon.Client/src/components/parameters/ParameterSelectionDrawer.vue
+++ Battelle.EPA.WideAreaDecon.Client/src/components/parameters/ParameterSelectionDrawer.vue
@@ -1,6 +1,8 @@
 <template>
   <v-navigation-drawer color="grey" class="lighten-2" clipped app :width="450">
-    <parameter-list-expansion-panel :list="parameters" />
+    <v-switch v-model="showMap" label="Geospatial Selection"></v-switch>
+    <geospatial-selection v-if="showMap" />
+    <parameter-list-expansion-panel :list="parameters" v-else />
   </v-navigation-drawer>
 </template>
 
@@ -8,16 +10,18 @@
 import Vue from 'vue';
 import { Component, Prop } from 'vue-property-decorator';
 import ParameterList from '@/implementations/parameter/ParameterList';
+import GeospatialSelection from './GeospatialSelection.vue';
 import ParameterListExpansionPanel from './ParameterListExpansionPanel.vue';
 
 @Component({
   components: {
     ParameterListExpansionPanel,
+    GeospatialSelection,
   },
 })
 export default class ParameterSelectionDrawer extends Vue {
   @Prop({ required: true }) parameters!: ParameterList;
+
+  showMap = false;
 }
 </script>
-
-<style scoped lang="scss"></style>
