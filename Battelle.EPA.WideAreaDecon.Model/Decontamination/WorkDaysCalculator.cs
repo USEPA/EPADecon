9e0b8286d6106cde1d2c106159bd68c9ea2735d8
emily <peraza@battelle.org>
2021-06-18 15:52:25 -0400

    Removed unused references

--- Battelle.EPA.WideAreaDecon.Model/Decontamination/WorkDaysCalculator.cs
+++ Battelle.EPA.WideAreaDecon.Model/Decontamination/WorkDaysCalculator.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Runtime.Serialization;
 using Battelle.EPA.WideAreaDecon.InterfaceData.Enumeration.Parameter;
 
 namespace Battelle.EPA.WideAreaDecon.Model.Decontamination
