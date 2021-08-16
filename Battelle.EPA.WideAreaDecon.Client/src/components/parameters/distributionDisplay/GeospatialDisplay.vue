2348f0d1ba5a707264b0b7d4574b1df8ecf13eff
Daniel Sharp <sharpd@battelle.org>
2021-08-16 16:01:03 -0400

    push Eli's latest changes to branch

--- Battelle.EPA.WideAreaDecon.Client/src/components/parameters/distributionDisplay/GeospatialDisplay.vue
+++ Battelle.EPA.WideAreaDecon.Client/src/components/parameters/distributionDisplay/GeospatialDisplay.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    <div id="map"></div>
+  <div id="map" ref="rootmap">
+    <div style="position: absolute; z-index: 9999; background: ghostwhite; display: flex">
+      <div>Geometry type &nbsp;</div>
+      <select v-model="typeSelected" @change="getTypeSelected">
+        <option value="Point">Point</option>
+        <option value="LineString">LineString</option>
+        <option value="Polygon">Polygon</option>
+        <option value="Circle">Circle</option>
+        <option value="None">None</option>
+      </select>
+    </div>
   </div>
 </template>
 <script lang="ts">
@@ -12,8 +21,11 @@ import { Component } from 'vue-property-decorator';
 import Map from "ol/Map";
 import View from "ol/View";
 import { defaults as defaultControls, ScaleLine } from "ol/control";
-import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
+import {Tile as TileLayer, Vector} from 'ol/layer';
+import VectorLayer from 'ol/layer/Vector';
 import {OSM, Vector as VectorSource} from 'ol/source';
+import { Style, Stroke } from 'ol/style';
+import Draw from 'ol/interaction/Draw';
 
 @Component
 export default class GeospatialDisplay extends Vue{
@@ -46,6 +58,37 @@ export default class GeospatialDisplay extends Vue{
         zoom: 2,
       }),
     });
+    // Add a layer for the drawn line
+    this.drawLayer = new VectorLayer({
+        //source corresponding to layer
+        source: new Vector(),
+
+    })
+    //Add layer to the map
+    this.map.addLayer(this.drawLayer);
+  }
+
+  getTypeSelected() {
+    this.draw && this.map.removeInteraction(this.draw);
+    //Draw a new Interaction based on the value of typeSelect
+    this.addInteraction();
+  }
+
+  addInteraction() {
+    let value = this.typeSelected;
+    if (value !== 'None') {
+      this.draw = new Draw({
+        source: this.drawLayer.getSource(),
+        type: this.typeSelected,
+        style: new Style({
+          stroke: new Stroke({
+            color: 'blue',
+            width: 3,
+          }),
+        }),
+      });
+      this.map.addInteraction(this.draw);
+    }
   }
 }
 </script>
