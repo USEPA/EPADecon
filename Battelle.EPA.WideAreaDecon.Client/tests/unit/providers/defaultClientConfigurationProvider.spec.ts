987338d7da1f0cc659d326cfd700912706c39693
Sam Stegman <stegman@battelle.org>
2022-02-23 14:59:53 -0500

    unit test fix

--- Battelle.EPA.WideAreaDecon.Client/tests/unit/providers/defaultClientConfigurationProvider.spec.ts
+++ Battelle.EPA.WideAreaDecon.Client/tests/unit/providers/defaultClientConfigurationProvider.spec.ts
@@ -1,6 +1,14 @@
 import { expect } from 'chai';
 import DefaultClientConfigurationProvider from '@/implementations/providers/DefaultClientConfigurationProvider';
 
+// refer to https://github.com/jsdom/jsdom/issues/1721 for createObjectURL issue
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+function noOp() {}
+
+if (typeof window.URL.createObjectURL === 'undefined') {
+  Object.defineProperty(window.URL, 'createObjectURL', { value: noOp });
+}
+
 describe('DefaultClientConfigurationProvider', function TestDefaultClientConfigurationProvider() {
   it('should have defaults of', async () => {
     // Setup
